506e02c655e28bff1215f4232c854957448cb85e
Ameliza S Correa <amelizacorrea@hotmail.com>
2018-03-01 17:23:29 -0300

    Soma (correção)

--- Soma.java
+++ Soma.java
@@ -7,7 +7,11 @@ public class Soma{
 		Scanner teclado = new Scanner(System.in);
 		int n1 = teclado.nextInt();
 		int n2 = teclado.nextInt();
-		System.out.println("O resultado de " + n1 + " + " + n2 + " é : " + (n1+n2));
+		
+		if(n1 >= n2)
+			System.out.println("O resultado de " + n1 + " + " + n2 + " é : " + (n1+n2));
+		else
+			System.out.println("O resultado de " + n1 + " - " + n2 + " é : " + (n1-n2));
 	}
 
 }
